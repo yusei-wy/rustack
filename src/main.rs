c849d7386a8ed7364026cb9df59fc32ac7ce2817
yusei-wy <yusei.kasa@gmail.com>
2025-01-06 18:41:04 +0900

    Add sub, mul, div func

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ fn main() {
                 Ok(parsed) => stack.push(parsed),
                 _ => match word {
                     "+" => add(&mut stack),
+                    "-" => sub(&mut stack),
+                    "*" => mul(&mut stack),
+                    "/" => div(&mut stack),
                     _ => panic!("{word:?} could not be parsed"),
                 },
             }
@@ -28,11 +31,48 @@ fn add(stack: &mut Vec<i32>) {
     stack.push(hls + lls);
 }
 
+fn sub(stack: &mut Vec<i32>) {
+    let hls = stack.pop().unwrap();
+    let lls = stack.pop().unwrap();
+    stack.push(hls - lls);
+}
+
+fn mul(stack: &mut Vec<i32>) {
+    let hls = stack.pop().unwrap();
+    let lls = stack.pop().unwrap();
+    stack.push(hls * lls);
+}
+
+fn div(stack: &mut Vec<i32>) {
+    let hls = stack.pop().unwrap();
+    let lls = stack.pop().unwrap();
+    stack.push(hls / lls);
+}
+
 mod tests {
     #[test]
     fn test_add() {
         let mut stack = vec![1, 2];
         super::add(&mut stack);
         assert_eq!(stack, vec![3]);
+    }
+
+    #[test]
+    fn test_sub() {
+        let mut stack = vec![1, 2];
+        super::sub(&mut stack);
+        assert_eq!(stack, vec![1]);
+    }
+    #[test]
+    fn test_mul() {
+        let mut stack = vec![1, 2];
+        super::mul(&mut stack);
+        assert_eq!(stack, vec![2]);
+    }
+    #[test]
+    fn test_div() {
+        let mut stack = vec![2, 4];
+        super::div(&mut stack);
+        assert_eq!(stack, vec![2]);
     }
 }
