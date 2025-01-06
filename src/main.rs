446a7a66b74687b510f71a431fe8207af7132084
yusei-wy <yusei.kasa@gmail.com>
2025-01-06 18:33:56 +0900

    Add read line

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
+use core::panic;
+
 fn main() {
-    let mut stack = vec![];
+    for line in std::io::stdin().lines() {
+        let mut stack = vec![];
 
-    stack.push(42);
-    stack.push(36);
+        let Ok(line) = line else {
+            continue;
+        };
 
-    println!("stack: {stack:?}");
+        for word in line.split_whitespace().collect::<Vec<_>>() {
+            match word.parse::<i32>() {
+                Ok(parsed) => stack.push(parsed),
+                _ => match word {
+                    "+" => add(&mut stack),
+                    _ => panic!("{word:?} could not be parsed"),
+                },
+            }
+        }
+
+        println!("stack: {stack:?}");
+    }
+}
+
+fn add(stack: &mut Vec<i32>) {
+    let hls = stack.pop().unwrap();
+    let lls = stack.pop().unwrap();
+    stack.push(hls + lls);
+}
+
+mod tests {
+    #[test]
+    fn test_add() {
+        let mut stack = vec![1, 2];
+        super::add(&mut stack);
+        assert_eq!(stack, vec![3]);
+    }
 }
